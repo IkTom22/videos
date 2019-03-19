7c3b1497946f3af4b382963634e2c500459c51f2
Iko <ikotominaga@gmail.com>
2019-03-19 16:11:17 +1300

    ui grid added and fefault video added

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,12 @@ class App extends React.Component {
 
     state = { videos: [], selectedVideo: null }
 
+    componentDidMount() {
+        this.onTermSubmit('sustanable house');
+
+    }
+
+
     // it's either 'async const response = await ..' method ot fetch method
     onTermSubmit = async (term) => {
         // updated term from SearchBar will be print out
@@ -18,7 +24,10 @@ class App extends React.Component {
             }
         });
         // of all the response fdata I get, I only need response.data.items
-        this.setState({ videos: response.data.items });
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
     // selected video item
     onVideoSelect = (video) => {
@@ -28,8 +37,19 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+
+                    </div>
+
+                </div>
+
 
             </div>
         );
